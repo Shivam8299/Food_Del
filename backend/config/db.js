91c1ea7586600a4b78f03c0adc889f2d68ca154e
Shivam8299 <shivammishra0381@gmail.com>
2025-02-25 11:36:08 +0530

    new commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectedDb  = async () => {
     try {
-        await mongoose.connect('mongodb+srv://shivam23:6392595578@cluster0.ixg0j.mongodb.net/?')
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log('db connected successfully')
     } catch (error) {
         console.log(error) 
