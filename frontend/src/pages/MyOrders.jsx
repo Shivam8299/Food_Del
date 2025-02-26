58aefb02d5ecb8ea4236ce3b14fec6d7ca9966ee
Shivam8299 <shivammishra0381@gmail.com>
2025-02-27 00:45:56 +0530

    new commit

--- frontend/src/pages/MyOrders.jsx
+++ frontend/src/pages/MyOrders.jsx
@@ -2,8 +2,10 @@ import React, { useContext, useEffect, useState } from 'react'
 import { StoreContext } from '../context/StoreContext'
 import axios from 'axios';
 import { assets } from '../assets/frontend_assets/assets';
+import ShowTracker from './ShowTracker';
 
 function MyOrders() {
+    const [tracker,setTracker] = useState(false)
     const [data, SetData] = useState([])
     const {backendUrl , token} = useContext(StoreContext);
     const fetchOrders = async()=>{
@@ -19,6 +21,8 @@ function MyOrders() {
         fetchOrders()
        }
     },[token])
+    // const orderStatus = data.map(e =>e.status)
+    // console.log(orderStatus)
   return (
     <div className='mx-1 sm:mx-2 md:mx-16 lg:mx-32'>
         <p className='text-xl md:text-2xl mb-6 font-bold text-center md:text-start lg:ml-2'>My orders</p>
@@ -37,7 +41,8 @@ function MyOrders() {
                     <p className='text-sm ml-8 md:ml-16 lg:ml-6 text-gray-700'>${order.amount}.00</p>
                     <p className='text-sm ml-2 md:ml-0 lg:ml-4 text-gray-700 font-serif'>items: {order.items.length}</p>
                     <p className='text-xs sm:text-sm font-serif  text-gray-800' ><span>&#x25cf;</span> <b>{order.status}</b></p>
-                    <button className='mx-4 md:mx-6 font-serif py-[6px] bg-red-100 text-xs md:text-sm rounded-sm hover:bg-red-400 hover:text-white cursor-pointer transition-all duration-300'>Track Order</button>
+                    <button  onClick={() => setTracker(setTracker === index ? null : index)}  className='mx-4 md:mx-6 font-serif py-[6px] bg-red-100 text-xs md:text-sm rounded-sm hover:bg-red-400 hover:text-white cursor-pointer transition-all duration-300'>Track Order</button>
+                    {tracker === index && <ShowTracker setTracker={setTracker} status = {order.status}/>}
                 </div>
             )
         })}
